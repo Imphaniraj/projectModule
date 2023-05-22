a827293ede543851b97d3f38a15aa68d02215e5c
Phani Raj <prallabhandi@groupon.com>
2023-05-22 21:36:43 +0530

    tskmgr

---
+++ tsk-mgr/src/main/java/com/phani/tskmgr/TskMgrApplication.java
@@ -0,0 +1,13 @@
+package com.phani.tskmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TskMgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TskMgrApplication.class, args);
+    }
+
+}
