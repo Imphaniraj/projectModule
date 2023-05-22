1087da408d484afdd170e4202e7a1fec5dc997b1
Phani Raj <prallabhandi@groupon.com>
2023-05-22 18:07:27 +0530

    calling fakeapi
    Signed-off-by: Phani Raj <prallabhandi@groupon.com>

--- java-basics/src/main/java/org/phani/Main.java
+++ java-basics/src/main/java/org/phani/Main.java
@@ -1,10 +1,36 @@
 package org.phani;
 
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String [] args){
-        System.out.println(Lib.getGreeting());
-        var httpLib = new HttpLib();
-        System.out.println(httpLib.getExampleDotCom());
+    public static void main(String [] args) throws IOException {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+        var api = retrofit.create(JSONPlaceholderAPI.class);
+        if (args.length == 0) {
+            System.out.println(Lib.getGreeting());
+        } else {
+            if (args[0].equals("example")) {
+                var httpLib = new HttpLib();
+                System.out.println(httpLib.getExampleDotCom());
+            }
+            if (args[0].equals("posts")) {
+                api.getPosts().execute().body().forEach(post -> {
+                    System.out.println(post.getTitle());
+                });
+            }
+
+            if (args[0].equals("photos")) {
+                api.getPhotos().execute().body().forEach(photo -> {
+                    System.out.println(photo.getUrl());
+                });
+            }
+        }
     }
 }
