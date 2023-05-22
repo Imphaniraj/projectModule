594710b751d78b84372c857b4768449247702caa
Phani Raj <prallabhandi@groupon.com>
2023-05-22 16:55:35 +0530

    get call
    Signed-off-by: Phani Raj <prallabhandi@groupon.com>

--- java-basics/src/main/java/org/phani/Main.java
+++ java-basics/src/main/java/org/phani/Main.java
@@ -4,5 +4,7 @@ package org.phani;
 public class Main {
     public static void main(String [] args){
         System.out.println(Lib.getGreeting());
+        var httpLib = new HttpLib();
+        System.out.println(httpLib.getExampleDotCom());
     }
 }
